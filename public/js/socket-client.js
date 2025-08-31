4c505311347575e4a29fa74bccac9bf5381b743c
jairom94 <jairom94@gmail.com>
2025-08-30 23:37:54 -0500

    add: style to chat, manage send message

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -11,9 +11,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const $ = (selector) => document.querySelector(selector);
   const usersOnline = $(".users-online");
   const txtMessage = $(".txt-message");
-  let userSelected = false;
+  let sessionUserId;
   const messages = $(".messages");  
   const btnSendMessage = $(".send-message");
+  if(messages instanceof HTMLUListElement){
+    sessionUserId = messages.dataset.user;
+  }
   if (txtMessage instanceof HTMLInputElement) {
     txtMessage.addEventListener("keyup", (e) => {});
   }
@@ -21,10 +24,10 @@ document.addEventListener("DOMContentLoaded", () => {
     socket.on("update-online-users", (usersOnlineList) => {
       if (Array.isArray(usersOnlineList)) {
         usersOnline.innerHTML = "";
-        const usersOnlineData = usersOnlineList.map((u) => ({
-          ...u,
-          select: false,
-        }));
+        // const usersOnlineData = usersOnlineList.map((u) => ({
+        //   ...u,
+        //   select: false,
+        // }));
         for (const user of usersOnlineList) {
           const li = createElement("li",{'data-user':user.id});
           li.classList.add(
@@ -40,22 +43,19 @@ document.addEventListener("DOMContentLoaded", () => {
           const span = createElement("span");
           span.textContent = user?.fullname;          
           li.appendChild(span);
-
-        //   const idUser = createElement('span')
-        //   idUser.textContent = user?.id
-        //   li.appendChild(idUser)
-          // li.textContent = user?.fullname
           usersOnline.appendChild(li);
           li.addEventListener("click", () => {
             usersOnline.querySelectorAll("li").forEach((el) => {
               el.classList.remove("bg-amber-600");
             });
             li.classList.add("bg-amber-600");
-            //Creacion de sala para chat
+            //Creación de sala para chat
             if (messages instanceof HTMLUListElement) {
-              const userID = li.dataset.user ?? "";
-              if (userID) {
-                socket.emit("start-private-chat", userID);
+              // const userID = li.dataset.user ?? "";              
+              if (user.id) {
+                if(sessionUserId && sessionUserId !== user.id){                  
+                  socket.emit("start-private-chat", user.id);
+                }
               }
             }
           });
@@ -63,15 +63,69 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   }
+  let currentChatRoomId;
+  if(btnSendMessage instanceof HTMLButtonElement){
+    btnSendMessage.addEventListener('click',()=>{
+      if(currentChatRoomId && txtMessage instanceof HTMLInputElement){
+        // console.log(currentChatRoomId);   
+        if(txtMessage.value){
+          sendMessagePrivate(txtMessage.value,currentChatRoomId)
+          messages.scrollTo({
+            top: messages.scrollHeight,
+            behavior: 'smooth'
+          });
+          txtMessage.value = ''
+        }     
+      }
+    })
+  }  
+  if(txtMessage instanceof HTMLInputElement){
+    txtMessage.addEventListener('keyup',(e)=>{
+      console.log(e.key);
+      
+    })
+  }
   socket.on('private-chat-started',(payload)=>{
-    console.log(payload);
-    socket.emit('send-private-message',{roomId:payload.roomId,content:'hola desde el navegador'})
+    // console.log(payload);   
+    currentChatRoomId = payload.roomId; 
   })  
   socket.on('new-private-message',(message)=>{
-    console.log(message);
-    
+    // console.log(message);
+    if(messages instanceof HTMLUListElement){
+      const msg = createElement('li')
+      msg.classList.add('relative')
+      if(message.author.id !== sessionUserId){
+        msg.classList.add('ml-auto')
+      }
+      msg.innerHTML = `
+      <span 
+      class="block absolute size-3 skew-x-[30deg] bg-amber-300 top-0 -left-[.15rem] z-0"></span>
+        <div class="w-fit bg-amber-300 rounded-lg px-2 py-1 text-xs z-10 relative">
+            <span>${message.content}</span>
+        </div>
+      <span class="text-xs text-gray-700">hace 2min</span>
+      `
+      // msg.textContent = message.content
+      messages.appendChild(msg)
+    }
   })
+ 
+  /**
+   * 
+   * @param {string} message 
+   * @param {string} roomId 
+   */
+  function sendMessagePrivate(message,roomId) {
+    if(message.trim() && currentChatRoomId){
+      socket.emit('send-private-message',{
+          roomId:roomId,
+          content:message
+      })
+    }
+  }
 });
+
+
 
 /**
  * Crea un elemento HTML con atributos personalizados.
